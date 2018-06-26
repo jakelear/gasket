aae855bd05c5dde53d127878e16f6e853bf85ead
Jake Lear <jakelear@gmail.com>
2018-06-25 23:38:22 -0400

    Fuzzy search

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
 const { GraphQLServer } = require('graphql-yoga')
 const fetch = require('node-fetch')
+const fuzzy = require('fuzzy');
+
 const baseURL = `https://api.steampowered.com`
 const resolvers = {
   Query: {
     applist: async (parent, args) => {
+      const matchThreshold = 20;
       const { filter } = args;
       const results = await fetch(`${baseURL}/ISteamApps/GetAppList/v2`);
       const json = await results.json();
-      json.applist.apps = json.applist.apps.filter((app) => app.name.toLowerCase().includes(filter.toLowerCase()))
-      return json.applist;
+
+      const filtered = fuzzy
+        .filter(filter, json.applist.apps, { extract: app => app.name })
+        .map(function(el) {
+          return {
+            'name': el.string,
+            'appid': el.original.appid,
+            'score': el.score
+          };
+        })
+        .filter(app => app.score > matchThreshold);
+
+      console.log(filtered);
+      return {
+        apps: filtered
+      };
     },
     appNews: (parent, args)  => {
       const { appid } = args;
